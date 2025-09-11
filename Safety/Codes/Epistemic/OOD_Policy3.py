d964a9538d16989b609245cdd11793d7a51bd426
Sebastien Gros <Sebastien.gros@ntnu.no>
2025-09-11 22:12:22 +0200

    Update OOD

--- Safety/Codes/Epistemic/OOD_Policy3.py
+++ Safety/Codes/Epistemic/OOD_Policy3.py
@@ -102,8 +102,8 @@ for case in ['Map','Star','OOD']:
         ax1.set_aspect('equal', 'box')
         ax1.set_xticks([])
         ax1.set_yticks([])
-        ax1.set_xlabel('State',fontsize=FS)
-        ax1.set_ylabel('Action',fontsize=FS)
+        ax1.set_xlabel('State',fontsize=20)
+        ax1.set_ylabel('Action',fontsize=20)
         ax1.contour(sv,av,Lplot['Star'],colors='k',linestyles='solid')
         ax1.plot(sref,aref,marker='o',color='b')
         
